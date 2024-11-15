511cb889d98732acc4f23813fb97e769f1689a27
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-11-16 03:23:40 +0700

    Initial Commit

---
+++ ExpanseTracker/ExpanseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpanseTrackerApp.swift
+//  ExpanseTracker
+//
+//  Created by Ricky Primayuda Putra on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpanseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
